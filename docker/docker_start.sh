b3a4c1cbadf29c99186354ffdb51855c7bb29360
sinopeta <acpt1994@gmail.com>
2020-07-03 12:23:04 +0200

    sharding impostato

--- docker/docker_start.sh
+++ docker/docker_start.sh
@@ -1,4 +1,3 @@
 systemctl start docker
 sudo docker start postgres_dev
-#sudo docker start maadb-mongo
 sudo docker-compose up -d
---
+++ sample/db_managment/show_mongo_status.py
@@ -0,0 +1,8 @@
+# from sample.db_managment.mongo_connection import mongo_connect
+# from pymongo import
+#
+# def status():
+#
+#     conn = mongo_connect('localhost', 27017)
+#
+#     conn.adminCommand({ listShards: 1 })
