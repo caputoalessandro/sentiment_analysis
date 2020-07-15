11af82610680cbd59ad32a0938c004cd19a57b3a
sinopeta <acpt1994@gmail.com>
2020-07-15 15:03:34 +0200

    plots sistemati

--- sample/stats/stats.py
+++ sample/stats/stats.py
@@ -1,23 +1,8 @@
 from sample.dao.get_dao import get_db
 import matplotlib.pyplot as plt
+import numpy as np
 
-
-def make_plot(sentiment, words, values):
-    plt.figure(figsize=(25, 12))
-    plt.subplot(132)
-    plt.bar(words, values)
-    plt.suptitle(sentiment)
-    plt.show()
-
-
-def postgres_calculate_percentage():
-    db = get_db("postgres")
-    twitter_lemmas = db.get_filtered_lemmas()
-
-    db = get_db("postgres")
-    resources_words = db.get_words()
-
-    sentiments = [
+sentiments = [
         "anger",
         "anticipation",
         "disgust",
@@ -27,6 +12,32 @@ def postgres_calculate_percentage():
         "surprise",
         "trust"
     ]
+
+
+def make_plot(sentiment, words, values):
+
+    plt.rcdefaults()
+    fig, ax = plt.subplots()
+
+    # Example data
+    y_pos = np.arange(len(words))
+
+    ax.barh(y_pos, values, align='center')
+    ax.set_yticks(y_pos)
+    ax.set_yticklabels(words)
+    ax.invert_yaxis()  # labels read top-to-bottom
+    ax.set_xlabel('Percentage %')
+    ax.set_title(sentiment)
+
+    plt.show()
+
+
+def mongo_calculate_percentage(n):
+    db = get_db("mongo")
+    twitter_lemmas = db.get_filtered_lemmas()
+
+    db = get_db("mongo")
+    resources_words = db.get_words()
 
     lemmas_number = {
         sentiment: len([
@@ -48,14 +59,24 @@ def postgres_calculate_percentage():
         for sentiment in sentiments
     }
 
-    w = []
-    v = []
-
     for sentiment, words in stats.items():
-        for word, value in words[:5]:
+        w = []
+        v = []
+
+        for word, value in words[:n]:
             w.append(word)
             v.append(value)
+
         make_plot(sentiment, w, v)
 
     return 0
 
+
+def mongo_calculate_percentage():
+    db = get_db("mongo")
+    twitter_lemmas = db.get_filtered_lemmas()
+
+    db = get_db("mongo")
+    resources_words = db.get_words()
+
+    breakpoint()
